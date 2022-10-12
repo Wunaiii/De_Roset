f8278b6bb1daa2e3c466a54574401a520de691ee
Alfredo Dirksen <alfredodirksen130@gmail.com>
2022-10-12 12:51:54 +0200

    nieuwe css sheet

--- php/config/database.php
+++ php/config/database.php
@@ -6,3 +6,11 @@ $dbname = "de_roset";
 $mysqli = new mysqli($host, $dbuser, $dbpass, $dbname);
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 ?>
+<html>
+<head>
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/gridstyle.css">
+</head>
+<body>
+</body>
+</html>
