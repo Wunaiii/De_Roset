dd4a9a732b60114c09a969638076884cbd83b86d
Alfredo Dirksen <alfredodirksen130@gmail.com>
2022-10-12 11:06:46 +0200

    grid aangewezen

--- php/homepage.php
+++ php/homepage.php
@@ -8,11 +8,12 @@
     </head>
     <body>
         <div class="container">
-            <div class="item-top item-1"> 1 </div>
-            <div class="item-top item-2"> 2 </div>
-            <div class="item-mid item-3"> 3 </div>
-            <div class="item-mid item-4"> 4 </div>
-            <div class="item-bot item-5"> 5 </div>
+            <div class="item-top item-1"> <h1> De Roset </h1> </div>
+            <div class="item-top item-2"> <h1>Navbar</h1> </div>
+            <div class="item-mid item-3"><h1> Smaak van de dag</h1> </div>
+            <div class="item-mid item-4"> <h1>Populaire smaken</h1> </div>
+            <div class="item-main item-5"> <h1>Blog</h1> </div>
+            <div class="item-bot item-6"> <h1>Footer</h1> </div>
         </div>
     </body>
 </html>
