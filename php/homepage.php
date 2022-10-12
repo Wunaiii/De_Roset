6634699a450e4567aef75c0484bab8f05827ef20
Alfredo Dirksen <alfredodirksen130@gmail.com>
2022-10-12 09:33:25 +0200

    grid proberen te fixen

---
+++ php/homepage.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/style.css">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    </head>
+    <body>
+        <div class="container">
+            <div class="item-top item-1"> 1 </div>
+            <div class="item-top item-2"> 2 </div>
+            <div class="item-mid item-3"> 3 </div>
+            <div class="item-mid item-4"> 4 </div>
+            <div class="item-bot item-5"> 5 </div>
+        </div>
+    </body>
+</html>
